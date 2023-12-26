3e178c03028ada99b596187177ff3698147112ab
ruby <rehab.khaled.bahnasy@gmail.com>
2023-12-26 15:46:00 +0200

    Start

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+*    author:  Crispy time limit
+        Abraam Ahmed Rehab
+**/
+
+#include <bits/stdc++.h>
+
+#define el '\n'
+#define Crispy ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    Crispy
+
+    return 0;
+}
